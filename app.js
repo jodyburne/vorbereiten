eb9e700f10d552d15ad98546980bfe97611654f0
Jodyburne <j__richardson@live.co.uk>
2019-08-13 15:34:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const data = require('./data')
+const app = express()
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+mongoose 
+    .connect('mongodb://localhost/interview', { useNewUrlParser: true })
+    .then(() => {
+        console.log('connected to mongo');
+    })
+    .catch(err => {
+        console.log('error with mongo connection', err)
+    });
+
+User.deleteMany().then(() => {
+    return User.create(data)
+}).then(docs => {
+    // console.log('ALL', docs)
+    console.log('AVAILABILTY') 
+    //docs.map( doc => doc.availability)
+})
+.catch(err => {
+    console.log('data creation error', err)
+})
+
+app.post('/new', (req, res, next) => {
+    let availabilityArr = []
+    if (req.body.monday) {
+        let mondayArr = req.body.monday.map(Number)
+        availabilityArr.push({'monday': mondayArr})
+    }
+    if (req.body.tuesday) {
+       let tuesdayArr = req.body.tuesday.map(Number)
+        availabilityArr.push({'tuesday': tuesdayArr}) 
+    }   
+    if (req.body.wednesday) {
+       let wednesdayArr = req.body.wednesday.map(Number)
+        availabilityArr.push({'wednesday': wednesdayArr})
+    }
+    if (req.body.thursday) {
+       let thursdayArr = req.body.thursday.map(Number)
+        availabilityArr.push({'thursday': thursdayArr}) 
+    }
+    if (req.body.friday) {
+       let fridayArr = req.body.friday.map(Number)
+        availabilityArr.push({'friday': fridayArr}) 
+    }
+    User.create({
+        name: req.body.name,
+        role: req.body.role,
+        availability: availabilityArr
+    })
+    .then(user => {
+        res.json(user)
+    }
+    )
+})
+
+app.get('/', (req, res, next) => {
+    User.find({role: 'candidate'})
+    .then(users => {
+        res.json(users)
+    })
+})
+app.post('/', (req, res, next) => {
+    let candidate = req.query.name
+
+Promise.all([
+ User.find({name: candidate}),
+ User.find({role: 'interviewer'}) 
+])
+.then(([candidate, interviewers]) => {
+//this returns array of objects
+// res.json(interviewers)
+let result =[]
+let candidateAvailability = candidate[0].availability
+for (let i = 0; i < interviewers.length; i++) {
+    let commonAvailability = {}
+    //returning one object after the other
+commonAvailability[interviewers[i].name] = getPossTimeSlots(candidateAvailability, interviewers[i].availability)
+result.push(commonAvailability)
+}
+res.json(result)
+
+
+})
+.catch(err => {
+    console.log('data sending error', err)
+})})
+app.listen(3000, () => {
+  console.log('interview app on port 3000')
+});
+
+function getPossTimeSlots(candidate, interviewer){
+ let availabilityC = candidate   
+ let availabilityI = interviewer
+let test = []
+for (let i = 0; i < availabilityC.length; i++) {
+  for (let j = 0; j < availabilityI.length; j++) {
+let result = {}
+    if (JSON.stringify(Object.keys(availabilityC[i])) === JSON.stringify(Object.keys(availabilityI[j]))) {
+      let result1 =
+        Object.values(availabilityC[i]).reduce(
+          (acc, cur) => acc.concat(cur),
+          []
+        )
+      let result2 = Object.values(availabilityI[j]).reduce(
+        (acc, cur) => acc.concat(cur),
+        []
+      )
+      let comHours = result1.filter(elem => result2.includes(elem))
+      if (comHours.length > 0) {
+     result[Object.keys(availabilityC[i])[0]] = comHours
+        test.push(result)
+      } 
+    }
+  }
+}
+return test 
+}
---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports =
+    [
+      {
+          name: 'Ines',
+       role:'interviewer',
+    availability: [
+        {'monday': [9, 10, 11, 12, 13, 14, 15]},
+        {'tuesday': [9, 10, 11, 12, 13, 14, 15]},
+        {'wednesday': [9, 10, 11, 12, 13, 14, 15]},
+        {'thursday': [9, 10, 11, 12, 13, 14, 15]},
+        {'friday': [9, 10, 11, 12, 13, 14, 15]},
+    ]
+    },
+
+    {
+    name: 'Ingrid',
+    role: 'interviewer',
+    availability: [
+        {
+            'monday': [12, 13, 14, 15, 16, 17]
+        },
+        {'tuesday': [9, 10, 11]},
+        {'wednesday': [12, 13, 14, 15, 16, 17]},
+        {'thursday': [9, 10, 11]},
+    ]
+    },
+
+    { 
+    name: 'Carl',
+    role: 'candidate',
+    availability: [
+        {'monday': [9,]},
+        {'tuesday': [9,]},
+        {'wednesday': [10, 11,]},
+        {'thursday': [9,]},
+        {'friday': [9,]},
+    ]
+    }
+    ]
